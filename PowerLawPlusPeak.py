495679a2025fac50be77046152c4d831b80c732e
Kaze Wong <kazewong.physics@gmail.com>
2021-09-03 18:07:23 -0400

    Full analysis

--- PowerLawPlusPeak.py
+++ PowerLawPlusPeak.py
@@ -17,8 +17,10 @@ key = random.PRNGKey(42)
 def truncated_power_law(x,alpha,xmin,xmax):
 	norm = (xmax**(1-alpha)-xmin**(1-alpha))/(1-alpha)
 	output = (x**-alpha)/norm
-	output = index_update(output,((x<xmin)+(x>xmax)),-jnp.inf)
+	output = index_update(output,((x<xmin)+(x>xmax)),0)
 	return output
+
+truncated_power_law = jit(truncated_power_law)
 
 @jit
 def gaussian(x,mean,sigma):
@@ -37,6 +39,7 @@ z_range = [0.,1]
 z_axis = jnp.linspace(z_range[0],z_range[1],10000)
 dVdz = jnp.array(Planck15.differential_comoving_volume(z_axis).value/1e9)
 
+@jit
 def redshift_distribution(z,kappa):
 	dVdz_local = jnp.interp(z,z_axis,dVdz)
 	norm_z = jnp.trapz((1+z_axis)**(kappa-1)*jnp.array(dVdz))
@@ -51,9 +54,10 @@ def combine_pdf(params,data):
 	p_z = redshift_distribution(z,params['kappa'])
 	return p_m1*p_q*p_z
 
-def population_likelihood(params, data):
-	combine_pdf = combine_pdf(params,data)
-	output = jnp.sum(jnp.log(combine_pdf))
+def population_likelihood(params, data, prior):
+	combine_pdf_local = combine_pdf(params,data)
+	selection_bias = evaluate_selection(params,selection_samples)
+	output = jnp.sum(jnp.log(jnp.mean(combine_pdf_local/prior/selection_bias,axis=1)))
 	if jnp.isfinite(output):
 		return output
 	else:
@@ -64,13 +68,13 @@ def population_likelihood(params, data):
 ########################################
 
 true_param = {}
-true_param['alpha'] = 2.
-true_param['xmin'] = 2.
-true_param['xmax'] = 100.
-true_param['mean'] = 30.
-true_param['sigma'] = 1.
-true_param['mixing'] = 0.5
-true_param['beta'] = 2.
+true_param['alpha'] = 2.63
+true_param['beta'] = 1.26
+true_param['xmin'] = 4.59
+true_param['xmax'] = 86.22
+true_param['mean'] = 33.07
+true_param['sigma'] = 5.69
+true_param['mixing'] = 0.1
 true_param['kappa'] = 0.
 
 N_sample = 1000
@@ -91,13 +95,14 @@ m1 = np.append(O12['mass1_source'][()],O3['injections/mass1_source'][()][O3_sele
 m2 = np.append(O12['mass2_source'][()],O3['injections/mass2_source'][()][O3_selection])
 z = np.append(O12['redshift'][()],O3['injections/redshift'][()][O3_selection])
 pdraw = np.append(O12['sampling_pdf'][()],O3['injections/sampling_pdf'][()][O3_selection])
+pdraw = pdraw/m1
 # !!! Remember to fix the Jacobian going from (m1,m2,z) -> (m1,q,z)
-samples = np.array([m1,m2/m1,z]).T
+selection_samples = np.array([m1,m2/m1,z]).T
 Ndraw = O3.attrs['total_generated']+7.1*1e7
 
 def evaluate_selection(params,data):
     likelihood = combine_pdf(params,data)
-    return np.sum(likelihood/pdraw)/Ndraw
+    return jnp.sum(likelihood/pdraw)/Ndraw
 
 ########################################
 # loading GWTC2 data
@@ -105,7 +110,34 @@ def evaluate_selection(params,data):
 
 data = np.load('./data/GWTC12_m1m2z_highsig.npz')
 posterior = data['posterior_sample']
-posteiror[...,1] = posterior[...,1]/posterior[...,0]
+posterior[...,1] = posterior[...,1]/posterior[...,0]
 prior = data['prior'][:,:,0] # !!! Remember to fix the Jacobian going from (m1,m2,z) -> (m1,q,z)
+prior = prior/posterior[:,:,0]
 N_event = prior.shape[0]
+
+########################################
+# Checking Gradient 
+########################################
+
+def make_param(alpha=2.63,beta=1.26,xmin=3.59,xmax=86.22,mixing=0.1,mean=33.07,sigma=5.69,kappa=0.):
+	param = {}
+	param['alpha'] = alpha
+	param['xmin'] = xmin
+	param['xmax'] = xmax
+	param['mean'] = mean
+	param['sigma'] = sigma
+	param['mixing'] = mixing
+	param['beta'] = beta
+	param['kappa'] = kappa
+	return param
+
+
+
+def compute_dLdt(alpha,beta,xmin,xmax,mixing,mean,sigma,kappa=0.):
+	param = make_param(alpha,beta,xmin,xmax,mixing,mean,sigma,kappa)
+
+	L = population_likelihood(param,posterior,prior)
+	dLdlambda = jnp.stack(list(grad(population_likelihood)(param,posterior,prior).values()))
+	dLdtheta = grad(population_likelihood,argnums=1)(param,posterior,prior)
+	return L, dLdtheta[None]/dLdlambda.reshape(-1,1,1,1)
 
