70285cbed4472a2d9a33e5d8fb0b98ae1697ec1c
Kaze Wong <kazewong.physics@gmail.com>
2021-10-04 17:36:11 -0400

    Fixed integration method in inner product. Original summing method contribute huge error

--- jaxgw/likelihood/utils.py
+++ jaxgw/likelihood/utils.py
@@ -9,4 +9,4 @@ def inner_product(h1, h2, frequency, PSD):
     #psd_interp = jnp.interp(frequency, PSD_frequency, PSD)
     df = frequency[1] - frequency[0]
     integrand = jnp.conj(h1)* h2 / PSD
-    return 4. * jnp.real(jnp.sum(integrand)*df)
+    return 4. * jnp.real(jnp.trapz(integrand,dx=df))
