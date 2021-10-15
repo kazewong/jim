9bb556614b804e6eda8f927dbaac533c96031e11
Kaze Wong <kazewong.physics@gmail.com>
2021-10-15 15:20:16 -0400

    Update HMC example

--- example/blackjax/HMC_injection.py
+++ example/blackjax/HMC_injection.py
@@ -12,23 +12,24 @@ from jaxgw.likelihood.detector_projection import construct_arm, detector_tensor,
 from jaxgw.likelihood.utils import inner_product
 from jaxgw.waveform.TaylorF2 import TaylorF2
 from jaxgw.waveform.IMRPhenomC import IMRPhenomC
-from jax import random, grad, jit, vmap, jacfwd, jacrev, value_and_grad
+from jax import random, grad, jit, vmap, jacfwd, jacrev, value_and_grad, pmap
 
 true_m1 = 3.
-true_m2 = 3.
-true_ld = 1000
+true_m2 = 2.99
+true_ld = 300
+
 
 
 injection_parameters = dict(
     mass_1=true_m1, mass_2=true_m2, a_1=0.0, a_2=0.0, luminosity_distance=true_ld, theta_jn=0.4, psi=2.659,
-    phase=1.3, geocent_time=1126259642.413, ra=1.375, dec=-1.2108)
+    phase=0, geocent_time=0, ra=1.375, dec=-1.2108)
+
 
 guess_parameters = dict(
-    mass_1=true_m1, mass_2=true_m2)#, luminosity_distance=400.)#, theta_jn=0.4, psi=2.659, ra=1.375, dec=-1.2108)
+    trans_M_tot=true_m1, trans_q=true_m2)#, luminosity_distance=400.)#, theta_jn=0.4, psi=2.659, ra=1.375, dec=-1.2108)
 
-#injection_parameters = dict(mass_1=36., mass_2=29., luminosity_distance=410.)
-#
-#guess_parameters = dict(mass_1=40., mass_2=25., luminosity_distance=400.)
+#guess_parameters = dict(trans_M_tot=jnp.log(true_m1+true_m2), trans_q=jnp.log(true_m2/true_m1)-jnp.log(1-true_m2/true_m1))
+guess_parameters = dict(m1=true_m1, m2=true_m2)
 
 
 
@@ -37,7 +38,7 @@ guess_parameters = dict(
 # sensitivity
 ifos = bilby.gw.detector.InterferometerList(['H1'])
 ifos.set_strain_data_from_power_spectral_densities(
-    sampling_frequency=2048, duration=16,
+    sampling_frequency=2048, duration=4,
     start_time=- 3)
 
 psd = ifos[0].power_spectral_density_array
@@ -59,8 +60,8 @@ H1_arm1 = construct_arm(H1_long, H1_lat, H1_xarm_tilt, H1_xarm_azimuth)
 H1_arm2 = construct_arm(H1_long, H1_lat, H1_yarm_tilt, H1_yarm_azimuth)
 
 H1 = detector_tensor(H1_arm1, H1_arm2)
-strain = waveform#get_detector_response(waveform,injection_parameters,H1)
-#strain = get_detector_response(waveform,injection_parameters,H1)
+#strain = waveform#get_detector_response(waveform,injection_parameters,H1)
+strain = get_detector_response(waveform,injection_parameters,H1)
 
 print('SNR of the event: '+str(np.sqrt(inner_product(strain,strain,psd_frequency,psd))))
 
@@ -68,26 +69,38 @@ print('SNR of the event: '+str(np.sqrt(inner_product(strain,strain,psd_frequency
 def jax_likelihood(params, data, data_f, PSD):
 	waveform = IMRPhenomC(data_f, params)
 #	waveform = TaylorF2(data_f, params)
-#	waveform = get_detector_response(waveform, params, H1)
+	waveform = get_detector_response(waveform, params, H1)
 	match_filter_SNR = inner_product(waveform, data, data_f, PSD)
 	optimal_SNR = inner_product(waveform, waveform, data_f, PSD)
 	return (-2*match_filter_SNR + optimal_SNR)/2#, match_filter_SNR, optimal_SNR
 
 @jit
-def Mq_to_m1m2(M_tot,q):
+def m1m2_to_Mq(m1,m2):
+	M_tot = jnp.log(m1+m2)
+	q = jnp.log(m2/m1)-jnp.log(1-m2/m1)
+	return M_tot, q
+
+@jit
+def Mq_to_m1m2(trans_M_tot,trans_q):
+	M_tot = jnp.exp(trans_M_tot)
+	q = 1./(1+jnp.exp(-trans_q))
 	m1 = M_tot/(1+q)
 	m2 = m1*q
-	inv_1pq = 1./(1+q)
-	Jac_det =  inv_1pq*(M*(inv_1pq-q/inv_1pq**2))+(q*inv_1pq**3)*M
-	return m1, m2, Jac_det
+#	Jac_det = M_tot/(1+q)**2*jnp.exp(trans_M_tot-trans_q)/(1+jnp.exp(-trans_q))**2
+	return m1, m2#, Jac_det
 
-def logprob_wrap(mass_1, mass_2):#, luminosity_distance):#, theta_jn, psi, ra, dec):
-#	m1, m2, Jac_det = Mq_to_m1m2(M_tot, q)
+#def logprob_wrap(trans_M_tot, trans_q):#, luminosity_distance):#, theta_jn, psi, ra, dec):
+@jit
+def logprob_wrap(m1, m2):#, luminosity_distance):#, theta_jn, psi, ra, dec):
+#	print(trans_M_tot,trans_q)
+#	m1, m2, Jac_det = Mq_to_m1m2(trans_M_tot, trans_q)
+#	m1, m2 = Mq_to_m1m2(trans_M_tot, trans_q)
 
-	params = dict(mass_1=mass_1, mass_2=mass_2, a_1=0, a_2=0, luminosity_distance=true_ld, theta_jn=0.4, psi=2.659, phase=1.3, geocent_time=1126259642.413, ra=1.375, dec=-1.2108)
-	return jax_likelihood(params, strain, psd_frequency, psd)
+	params = dict(mass_1=m1, mass_2=m2, a_1=0, a_2=0, luminosity_distance=true_ld, theta_jn=0.4, psi=2.659, phase=0, geocent_time=0, ra=1.375, dec=-1.2108)
+	return jax_likelihood(params, strain, psd_frequency, psd)#*Jac_det
 
 log_prob = lambda x: logprob_wrap(**x)
+log_prob = jit(log_prob)
 
 #def log_prob(param):
 #	if (param[0]<=0) or (param[1]<=0):
@@ -125,20 +138,20 @@ print('Finding step size and mass matrix')
 
 time1 = time.time()
 kernel_generator = lambda step_size, inverse_mass_matrix: hmc.kernel(
-    log_prob, step_size, inverse_mass_matrix, 60
+    log_prob, step_size, inverse_mass_matrix, 100
 )
 
 final_state, (step_size, inverse_mass_matrix), info = stan_warmup.run(
     key,
     kernel_generator,
     initial_state,
-    1000,
-    is_mass_matrix_diagonal=False
+    300,
+    #is_mass_matrix_diagonal=False
 )
 
 print("Finding inverse mass matrix takes: "+str(time.time()-time1)+" seconds")
 print("Stepsize: "+str(step_size))
-print("Mass_matrix: "+str(inverse_mass_matrix))
+print("Inverse mass matrix: "+str(inverse_mass_matrix))
 num_integration_steps = 60
 
 hmc_kernel = hmc.kernel(log_prob, step_size, inverse_mass_matrix, num_integration_steps)
