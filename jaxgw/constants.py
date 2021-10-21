e36ef6b82549d2daf953506f70511b3d99695121
Kaze Wong <kazewong.physics@gmail.com>
2021-10-21 12:18:26 -0400

    Collect constant into constant script

--- jaxgw/constants.py
+++ jaxgw/constants.py
@@ -5,4 +5,7 @@ from astropy.cosmology import WMAP9 as cosmo
 Msun = 4.9255e-6
 year = (1*yr).cgs.value
 Mpc = 1e6*pc.value/c.value
+euler_gamma = 0.577215664901532860606512090082
+MR_sun = 1.476625061404649406193430731479084713e3
+
 
