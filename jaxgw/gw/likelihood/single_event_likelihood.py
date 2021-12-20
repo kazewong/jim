af2644589c4c83c941444a9fe6b8abdfba53b056
Kaze Wong <kazewong.physics@gmail.com>
2021-12-20 12:48:55 -0500

    Dependencies fix in single_event_likelihood.py

--- jaxgw/gw/likelihood/single_event_likelihood.py
+++ jaxgw/gw/likelihood/single_event_likelihood.py
@@ -1,6 +1,6 @@
 from jax import jit
-from jaxgw.likelihood.detector_projection import get_detector_response
-from jaxgw.likelihood.utils import inner_product
+from jaxgw.gw.likelihood.detector_projection import get_detector_response
+from jaxgw.gw.likelihood.utils import inner_product
 
 def single_detector_likelihood(waveform_model, params, data, data_f, PSD, detector):
     waveform = waveform_model(data_f, params)
