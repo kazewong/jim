4649b234c77feec25e1770dcb3f5d77b4561e1c9
Kaze Wong <kazewong.physics@gmail.com>
2023-01-02 18:35:34 -0500

    Use HDI for pp plot

--- example/ParameterEstimation/make_ppPlot.py
+++ example/ParameterEstimation/make_ppPlot.py
@@ -1,32 +1,48 @@
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import minimize_scalar
+import arviz as az
+from scipy.interpolate import interp1d
+
+q_axis = np.linspace(0.1,1,100)
+eta = q_axis/(1+q_axis)**2
+q_interp = interp1d(eta,q_axis)
 
 def get_all_quantile(filename):
     data = np.load(filename)
     
     chains = data['chains']
     true_param = data['true_param']
-    chains[:,:,1] = chains[:,:,1]/(1+chains[:,:,1])**2
-    chains[:,:,7] = np.arccos(chains[:,:,7])
-    chains[:,:,10] = np.arcsin(chains[:,:,10])
-    
-    median = np.log10(0.5)
+    true_param[1] = q_interp(true_param[1])
+    true_param[7] = np.cos(true_param[7])
+    true_param[10] = np.sin(true_param[10])
+
     def compute_percentile(value,data):
-        f = lambda x : np.abs(np.quantile(data, 10**x) - value)
-        result = minimize(f,median,method="Nelder-Mead",bounds=[[-4,0]])
-        return np.abs(10**result.x[0]-0.5)*2
+        f = lambda x : np.min(np.abs(az.hdi(data, hdi_prob=x)-value))
+        result = minimize_scalar(f,bounds=[0.001,0.99],method='bounded')
+        return result.x
+
+
+    # Multi-modal HDI works only for data that are actuall multi-modal.
+    # If it is not, it may actually screw up the result
+    def compute_percentile_multimodal(value,data):
+        f = lambda x : np.min(np.abs(az.hdi(data, hdi_prob=x,multimodal=True,max_modes=4)-value))
+        result = minimize_scalar(f,bounds=[0.001,0.99],method='bounded')
+        return result.x
     
-    result = [] 
+    result = []
+    result_multimodal = []
     for i in range(11):
-        result.append(compute_percentile(true_param[i],chains[:,:,i]))
+        result.append(compute_percentile(true_param[i],chains[:,:,i].reshape(-1)[::10]))
+        result_multimodal.append(compute_percentile_multimodal(true_param[i],chains[:,:,i].reshape(-1)[::10]))
 
     mean_local_accs = data['local_accs'].mean()
     mean_global_accs = data['global_accs'].mean()
 
-    return np.array(result), true_param, mean_global_accs, mean_local_accs
+    return np.array(result), np.array(result_multimodal), true_param, mean_global_accs, mean_local_accs
 
 directory = '/mnt/home/wwong/ceph/GWProject/JaxGW/RealtimePE/ppPlots/balance_1001/'
 result = []
+result_multimodal = []
 true_param = []
 mean_global_accs = []
 mean_local_accs = []
@@ -34,13 +50,15 @@ for i in range(960):
     name = directory+'injection_'+str(i)+'.npz'
     local_result = get_all_quantile(name)
     result.append(local_result[0])
-    true_param.append(local_result[1])
-    mean_global_accs.append(local_result[2])
-    mean_local_accs.append(local_result[3])
+    result_multimodal.append(local_result[1])
+    true_param.append(local_result[2])
+    mean_global_accs.append(local_result[3])
+    mean_local_accs.append(local_result[4])
 
 result = np.stack(result)
+result_multimodal = np.stack(result_multimodal)
 true_param = np.stack(true_param)
 mean_global_accs = np.stack(mean_global_accs)
 mean_local_accs = np.stack(mean_local_accs)
 
-np.savez('/mnt/home/wwong/ceph/GWProject/JaxGW/RealtimePE/ppPlots/combined_quantile_balance_1001',result=result, true_param=true_param, mean_global_accs=mean_global_accs, mean_local_accs= mean_local_accs)
+np.savez('/mnt/home/wwong/ceph/GWProject/JaxGW/RealtimePE/ppPlots/combined_quantile_balance_1001',result=result, result_multimodal=result_multimodal, true_param=true_param, mean_global_accs=mean_global_accs, mean_local_accs= mean_local_accs)
