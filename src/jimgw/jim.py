fa206062a8c52e113ad3f8daadaab766ed786b61
Kaze Wong <kazewong.physics@gmail.com>
2023-07-05 21:41:51 -0400

    Update jim

--- src/jimgw/jim.py
+++ src/jimgw/jim.py
@@ -1,12 +1,13 @@
 from abc import ABC, abstractmethod
 from jaxtyping import Array
 from jimgw.likelihood import LikelihoodBase
-from flowMC.
+from flowMC.sampler.Sampler import Sampler
 
 class Jim(object):
     """ Master class for interfacing with flowMC
     
     """
 
-    def __init__(self, ):
+    def __init__(self, Sampler: Sampler, Likelihood: LikelihoodBase, **kwargs):
         pass
+
