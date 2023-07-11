0a3a5cf308b48131d0725bf3c6bcfbb294822b34
kazewong <kazewong.physics@gmail.com>
2023-07-11 11:08:38 -0400

    Make prior abstract class

--- src/jimgw/prior.py
+++ src/jimgw/prior.py
@@ -1,19 +1,38 @@
-from flowMC.nfmodel.base import Distribution
 import jax
+from flowMC.nfmodel.base import Distribution
 from jaxtyping import Array, Float
+from typing import Callable
 
-class Uniform(Distribution):
+class Prior(Distribution):
+    r"""A thin wrapper build on top of flowMC distributions to do book keeping.
+
+    Should not be used directly since it does not implement any of the real method.
+    """
+
+    naming: list[str]
+    transforms: list[Callable] = []
+
+    def __init__(self, naming: list[str], transforms: list[Callable] = []):
+        pass
+
+    def transform(self, x: Array):
+        pass
+
+
+class Uniform(Prior):
 
     xmin: Array
     xmax: Array
 
-    def __init__(self, xmin: float, xmax: float):
+    def __init__(self, xmin: float, xmax: float, naming: list[str]):
         self.xmax = xmax
         self.xmin = xmin
+        self.naming = naming
     
     def sample(self, rng_key: jax.random.PRNGKey, n_samples: int) -> Array:
-        return super().sample(rng_key, n_samples)
-    
+        samples = jax.random.uniform(rng_key, n_samples, minval=self.xmin, maxval=self.xmax)
+        return samples # TODO: remember to cast this to a named array
+
     def log_prob(self, x: Array) -> Float:
-        return super().log_prob(x)
+        return 1./(self.xmax-self.xmin)
     
