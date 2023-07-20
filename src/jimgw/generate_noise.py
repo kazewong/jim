7fed8ebae9a1bd4cf8d376ac9454093d33d696a1
tedwards2412 <tedwards2412@gmail.com>
2023-07-19 20:06:33 -0400

    Correctly generates noise in both the frequency and time domain

--- src/jimgw/generate_noise.py
+++ src/jimgw/generate_noise.py
@@ -1,23 +1,27 @@
 # Import packages
 from typing import List, Tuple
-import lalsimulation as lalsim 
 import jax.numpy as jnp
 import jax
 import numpy as np
-jax.config.update('jax_enable_x64', True)
 
-psd_func_dict = {
-    'H1': lalsim.SimNoisePSDaLIGOZeroDetHighPower,
-    'L1': lalsim.SimNoisePSDaLIGOZeroDetHighPower,
-    'V1': lalsim.SimNoisePSDAdvVirgo,
-}
-
-def generate_noise(seed: int, f_sampling: int = 2048, duration: int = 4, f_min: float = 30., ifos: List = ['H1', 'L1']):
+# This is needed for the noise generation to have enough precision to work
+jax.config.update("jax_enable_x64", True)
 
 
+def generate_fd_noise(
+    seed: int,
+    f_sampling: int = 2048,
+    duration: int = 4,
+    f_min: float = 30.0,
+    psd_funcs: dict = {
+        "H1": None,
+    },
+):
+    """
+    Generate frequency domain noise for a given set of detectors or specific PSD.
+    """
     # define sampling rate and duration
-
-    delta_t = 1/f_sampling
+    delta_t = 1 / f_sampling
     tlen = int(round(duration / delta_t))
 
     freqs = np.fft.rfftfreq(tlen, delta_t)
@@ -27,17 +31,17 @@ def generate_noise(seed: int, f_sampling: int = 2048, duration: int = 4, f_min: 
     # prescription to do so smoothly, but this is not really needed: you
     # could just set all values below `fmin` to a constant.
     def pad_low_freqs(f, psd_ref):
-        return psd_ref + psd_ref*(f_min-f)*jnp.exp(-(f_min-f))/3
+        return psd_ref + psd_ref * (f_min - f) * jnp.exp(-(f_min - f)) / 3
 
     psd_dict = {}
-    for ifo in ifos:
+    for ifo in psd_funcs.keys():
         psd = np.zeros(len(freqs))
-        for i,f in enumerate(freqs):
+        for i, f in enumerate(freqs):
             if f >= f_min:
-                psd[i] = psd_func_dict[ifo](f)
+                psd[i] = psd_funcs[ifo](f)
             else:
-                psd[i] = pad_low_freqs(f, psd_func_dict[ifo](f_min))
-        psd_dict[ifo] = jnp.array(psd,dtype=jnp.float64)
+                psd[i] = pad_low_freqs(f, psd_funcs[ifo](f_min))
+        psd_dict[ifo] = jnp.array(psd, dtype=jnp.float64)
 
     rng_key = jax.random.PRNGKey(seed)
     rng_keys = jax.random.split(rng_key)
@@ -45,9 +49,38 @@ def generate_noise(seed: int, f_sampling: int = 2048, duration: int = 4, f_min: 
     noise_fd_dict = {}
     for ifo, psd in psd_dict.items():
         rng_keys = jax.random.split(rng_keys[0], 3)
-        var = psd / (4.*delta_f)  # this is the variance of LIGO noise given the definition of the likelihood function
-        noise_real = jax.random.normal(rng_keys[1],shape=(len(psd),))*jnp.sqrt(var)
-        noise_imag = jax.random.normal(rng_keys[2],shape=(len(psd),))*jnp.sqrt(var)
-        noise_fd_dict[ifo] = noise_real + 1j*noise_imag
+        # this is the variance of LIGO noise given the definition of the likelihood function
+        var = psd / (4.0 * delta_f)
+        noise_real = jax.random.normal(rng_keys[1], shape=(len(psd),)) * jnp.sqrt(var)
+        noise_imag = jax.random.normal(rng_keys[2], shape=(len(psd),)) * jnp.sqrt(var)
+        noise_fd_dict[ifo] = noise_real + 1j * noise_imag
 
     return freqs, psd_dict, noise_fd_dict
+
+
+def generate_td_noise(
+    seed: int,
+    f_sampling: int = 2048,
+    duration: int = 4,
+    f_min: float = 30.0,
+    psd_funcs: dict = {
+        "H1": None,
+    },
+):
+    """
+    Generate time domain noise for a given set of detectors or specific PSD.
+    """
+
+    delta_t = 1 / f_sampling
+    tlen = int(round(duration / delta_t))
+    ts = jnp.linspace(0, duration, tlen)
+
+    _, psd_dict, noise_fd_dict = generate_fd_noise(
+        seed, duration=duration, f_sampling=f_sampling, psd_funcs=psd_funcs, f_min=f_min
+    )
+
+    noise_td_dict = {}
+    for ifo, psd in noise_fd_dict.items():
+        noise_td_dict[ifo] = jnp.fft.irfft(noise_fd_dict[ifo]) * f_sampling
+
+    return ts, noise_td_dict
