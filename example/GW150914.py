bc92b3c8bafb64571d4696983fbcd5f71b3cba6d
Kaze Wong <kazewong.physics@gmail.com>
2024-05-04 13:22:27 -0400

    update config

--- example/GW150914.py
+++ example/GW150914.py
@@ -105,8 +105,8 @@ local_sampler_arg = {"step_size": mass_matrix * 3e-3}
 Adam_optimizer = optimization_Adam(n_steps=3000, learning_rate=0.01, noise_level=1)
 
 import optax
-n_epochs = 50
-n_loop_training = 50
+n_epochs = 5
+n_loop_training = 600
 total_epochs = n_epochs * n_loop_training
 start = total_epochs//10
 learning_rate = optax.polynomial_schedule(
@@ -118,9 +118,9 @@ jim = Jim(
     likelihood,
     prior,
     n_loop_training=n_loop_training,
-    n_loop_production=10,
-    n_local_steps=150,
-    n_global_steps=150,
+    n_loop_production=20,
+    n_local_steps=10,
+    n_global_steps=100,
     n_chains=500,
     n_epochs=n_epochs,
     learning_rate=learning_rate,
