c3446b75389e94efeb0a57b2effba24d4f834358
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2024-07-24 16:29:46 -0400

    Add test for spin transform

--- test/verify_transform.py
+++ test/verify_transform.py
@@ -1,4 +1,5 @@
 import numpy as np
+import jax.numpy as jnp
 
 class VerifyTransform:
     def verify_sky_location_transform(self):
@@ -14,8 +15,36 @@ class VerifyTransform:
 
         import matplotlib.pyplot as plt
 
-        for zenith in np.linspace(0, np.pi, 5):
-            for azimuth in np.linspace(0, 2*np.pi, 5):
+        for zenith in np.linspace(0, np.pi, 10):
+            for azimuth in np.linspace(0, 2*np.pi, 10):
                 bilby_sky_location = np.array(bilby_earth_to_sky(zenith, azimuth, geocent_time, InterferometerList(ifos)))
                 jimgw_sky_location = np.array(jimgw_earth_to_sky(zenith, azimuth, Time(geocent_time, format="gps").sidereal_time("apparent", "greenwich").rad, detector_preset[ifos[0]].vertex - detector_preset[ifos[1]].vertex))
                 assert np.allclose(bilby_sky_location, jimgw_sky_location, atol=1e-4)
+
+    def verify_spin_transform(self):
+        from bilby.gw.conversion import bilby_to_lalsimulation_spins as bilby_spin_transform
+        from bilby.gw.conversion import symmetric_mass_ratio_to_mass_ratio, chirp_mass_and_mass_ratio_to_component_masses
+        from jimgw.constants import Msun
+
+        from jimgw.single_event.utils import spin_to_cartesian_spin as jimgw_spin_transform
+
+        for _ in range(100):
+            thetaJN = jnp.array(np.random.uniform(0, np.pi))
+            phiJL = jnp.array(np.random.uniform(0, np.pi))
+            theta1 = jnp.array(np.random.uniform(0, np.pi))
+            theta2 = jnp.array(np.random.uniform(0, np.pi))
+            phi12 = jnp.array(np.random.uniform(0, np.pi))
+            chi1 = jnp.array(np.random.uniform(0, 1))
+            chi2 = jnp.array(np.random.uniform(0, 1))
+            M_c = jnp.array(np.random.uniform(1, 100))
+            eta = jnp.array(np.random.uniform(0.1, 0.25))
+            fRef = jnp.array(np.random.uniform(10, 1000))
+            phiRef = jnp.array(np.random.uniform(0, 2*np.pi))
+
+            q = symmetric_mass_ratio_to_mass_ratio(eta)
+            m1, m2 = chirp_mass_and_mass_ratio_to_component_masses(M_c, q)
+            bilby_spin = jnp.array(bilby_spin_transform(thetaJN, phiJL, theta1, theta2, phi12, chi1, chi2, m1*Msun, m2*Msun, fRef, phiRef))
+            jimgw_spin = jnp.array(jimgw_spin_transform(thetaJN, phiJL, theta1, theta2, phi12, chi1, chi2, M_c, eta, fRef, phiRef))
+            assert np.allclose(bilby_spin, jimgw_spin)
+
+# VerifyTransform().verify_spin_transform()
