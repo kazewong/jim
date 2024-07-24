e6e45efc00b1790a4e0d8ef087aab51bf627472d
kazewong <kazewong.physics@gmail.com>
2024-07-24 15:46:19 -0400

    scaffold prior test

--- test/test_prior.py
+++ test/test_prior.py
@@ -1,1 +1,16 @@
-from jimgw.prior import Composite, Unconstrained_Uniform, Uniform
+from jimgw.prior import *
+
+
+class TestUnivariatePrior:
+    def test_logit(self):
+        p = Logit()
+
+class TestPriorOperations:
+    def test_combine(self):
+        raise NotImplementedError
+    
+    def test_sequence(self):
+        raise NotImplementedError
+    
+    def test_factor(self):
+        raise NotImplementedError
