ed727afeef73eb1c51b3400c30a2f63eec95464a
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-08-01 21:16:12 -0400

    Updated test_prior.py

--- test/unit/test_prior.py
+++ test/unit/test_prior.py
@@ -30,7 +30,7 @@ class TestUnivariatePrior:
         samples = p.sample(jax.random.PRNGKey(0), 10000)
         assert jnp.all(jnp.isfinite(samples['x']))
         # Check that the log_prob is correct in the support
-        samples = trace_prior_parent(p, [])[0].sample(jax.random.PRNGKey(0), 10000)
+        samples = p.sample(jax.random.PRNGKey(0), 10000)
         log_prob = jax.vmap(p.log_prob)(samples)
         assert jnp.allclose(log_prob, jnp.log(1.0 / (xmax - xmin)))
 
@@ -40,7 +40,7 @@ class TestUnivariatePrior:
         samples = p.sample(jax.random.PRNGKey(0), 10000)
         assert jnp.all(jnp.isfinite(samples['x']))
         # Check that the log_prob is finite
-        samples = trace_prior_parent(p, [])[0].sample(jax.random.PRNGKey(0), 10000)
+        samples = p.sample(jax.random.PRNGKey(0), 10000)
         log_prob = jax.vmap(p.log_prob)(samples)
         assert jnp.all(jnp.isfinite(log_prob))
         # Check that the log_prob is correct in the support
@@ -48,7 +48,7 @@ class TestUnivariatePrior:
         y = jax.vmap(p.base_prior.base_prior.transform)(x)
         y = jax.vmap(p.base_prior.transform)(y)
         y = jax.vmap(p.transform)(y)
-        assert jnp.allclose(jax.vmap(p.log_prob)(x), jnp.log(jnp.sin(y['x'])/2.0))
+        assert jnp.allclose(jax.vmap(p.log_prob)(y), jnp.log(jnp.sin(y['x'])/2.0))
         
     def test_cosine(self):
         p = CosinePrior(["x"])
@@ -56,14 +56,14 @@ class TestUnivariatePrior:
         samples = p.sample(jax.random.PRNGKey(0), 10000)
         assert jnp.all(jnp.isfinite(samples['x']))
         # Check that the log_prob is finite
-        samples = trace_prior_parent(p, [])[0].sample(jax.random.PRNGKey(0), 10000)
+        samples = p.sample(jax.random.PRNGKey(0), 10000)
         log_prob = jax.vmap(p.log_prob)(samples)
         assert jnp.all(jnp.isfinite(log_prob))
         # Check that the log_prob is correct in the support
         x = trace_prior_parent(p, [])[0].add_name(jnp.linspace(-10.0, 10.0, 1000)[None])
         y = jax.vmap(p.base_prior.transform)(x)
         y = jax.vmap(p.transform)(y)
-        assert jnp.allclose(jax.vmap(p.log_prob)(x), jnp.log(jnp.cos(y['x'])/2.0))
+        assert jnp.allclose(jax.vmap(p.log_prob)(y), jnp.log(jnp.cos(y['x'])/2.0))
 
     def test_uniform_sphere(self):
         p = UniformSpherePrior(["x"])
