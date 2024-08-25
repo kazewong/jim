5d6a795616003ded35814babc018bf6564f489e4
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-08-25 23:02:51 +0800

    Added run script

--- example/GW150914_PV2.py
+++ example/GW150914_PV2.py
@@ -4,12 +4,14 @@ import jax
 import jax.numpy as jnp
 
 from jimgw.jim import Jim
-from jimgw.prior import Composite, Sphere, Unconstrained_Uniform
+from jimgw.prior import CombinePrior, UniformPrior, CosinePrior, SinePrior, PowerLawPrior
 from jimgw.single_event.detector import H1, L1
 from jimgw.single_event.likelihood import TransientLikelihoodFD
 from jimgw.single_event.waveform import RippleIMRPhenomPv2
+from jimgw.transforms import BoundToUnbound
+from jimgw.single_event.transforms import MassRatioToSymmetricMassRatioTransform, SpinToCartesianSpinTransform, ComponentMassesToChirpMassMassRatioTransform, SkyFrameToDetectorFrameSkyPositionTransform, DistanceToSNRWeightedDistanceTransform, GeocentricArrivalTimeToDetectorArrivalTimeTransform, GeocentricArrivalPhaseToDetectorArrivalPhaseTransform
+from jimgw.single_event.utils import Mc_q_to_m1_m2
 from flowMC.strategy.optimization import optimization_Adam
-
 
 jax.config.update("jax_enable_x64", True)
 
@@ -21,125 +23,114 @@ total_time_start = time.time()
 
 # first, fetch a 4s segment centered on GW150914
 gps = 1126259462.4
-start = gps - 2
-end = gps + 2
+duration = 4
+post_trigger_duration = 2
+start_pad = duration - post_trigger_duration
+end_pad = post_trigger_duration
 fmin = 20.0
 fmax = 1024.0
 
-ifos = ["H1", "L1"]
+ifos = [H1, L1]
 
-H1.load_data(gps, 2, 2, fmin, fmax, psd_pad=16, tukey_alpha=0.2)
-L1.load_data(gps, 2, 2, fmin, fmax, psd_pad=16, tukey_alpha=0.2)
+f_ref = 20.0
 
-waveform = RippleIMRPhenomPv2(f_ref=20)
+for ifo in ifos:
+    ifo.load_data(gps, start_pad, end_pad, fmin, fmax, psd_pad=16, tukey_alpha=0.2)
 
-###########################################
-########## Set up priors ##################
-###########################################
+M_c_min, M_c_max = 10.0, 80.0
+q_min, q_max = 0.125, 1.0
+m_1_prior = UniformPrior(Mc_q_to_m1_m2(M_c_min, q_max)[0], Mc_q_to_m1_m2(M_c_max, q_min)[0], parameter_names=["m_1"])
+m_2_prior = UniformPrior(Mc_q_to_m1_m2(M_c_min, q_min)[1], Mc_q_to_m1_m2(M_c_max, q_max)[1], parameter_names=["m_2"])
+theta_jn_prior = SinePrior(parameter_names=["theta_jn"])
+phi_jl_prior = UniformPrior(0.0, 2 * jnp.pi, parameter_names=["phi_jl"])
+theta_1_prior = SinePrior(parameter_names=["theta_1"])
+theta_2_prior = SinePrior(parameter_names=["theta_2"])
+phi_12_prior = UniformPrior(0.0, 2 * jnp.pi, parameter_names=["phi_12"])
+a_1_prior = UniformPrior(0.0, 1.0, parameter_names=["a_1"])
+a_2_prior = UniformPrior(0.0, 1.0, parameter_names=["a_2"])
+dL_prior = PowerLawPrior(10.0, 2000.0, 2.0, parameter_names=["d_L"])
+t_c_prior = UniformPrior(-0.05, 0.05, parameter_names=["t_c"])
+phase_c_prior = UniformPrior(0.0, 2 * jnp.pi, parameter_names=["phase_c"])
+psi_prior = UniformPrior(0.0, jnp.pi, parameter_names=["psi"])
+ra_prior = UniformPrior(0.0, 2 * jnp.pi, parameter_names=["ra"])
+dec_prior = CosinePrior(parameter_names=["dec"])
 
-Mc_prior = Unconstrained_Uniform(10.0, 80.0, naming=["M_c"])
-q_prior = Unconstrained_Uniform(
-    0.125,
-    1.0,
-    naming=["q"],
-    transforms={"q": ("eta", lambda params: params["q"] / (1 + params["q"]) ** 2)},
-)
-s1_prior = Sphere(naming="s1")
-s2_prior = Sphere(naming="s2")
-dL_prior = Unconstrained_Uniform(0.0, 2000.0, naming=["d_L"])
-t_c_prior = Unconstrained_Uniform(-0.05, 0.05, naming=["t_c"])
-phase_c_prior = Unconstrained_Uniform(0.0, 2 * jnp.pi, naming=["phase_c"])
-cos_iota_prior = Unconstrained_Uniform(
-    -1.0,
-    1.0,
-    naming=["cos_iota"],
-    transforms={
-        "cos_iota": (
-            "iota",
-            lambda params: jnp.arccos(
-                jnp.arcsin(jnp.sin(params["cos_iota"] / 2 * jnp.pi)) * 2 / jnp.pi
-            ),
-        )
-    },
-)
-psi_prior = Unconstrained_Uniform(0.0, jnp.pi, naming=["psi"])
-ra_prior = Unconstrained_Uniform(0.0, 2 * jnp.pi, naming=["ra"])
-sin_dec_prior = Unconstrained_Uniform(
-    -1.0,
-    1.0,
-    naming=["sin_dec"],
-    transforms={
-        "sin_dec": (
-            "dec",
-            lambda params: jnp.arcsin(
-                jnp.arcsin(jnp.sin(params["sin_dec"] / 2 * jnp.pi)) * 2 / jnp.pi
-            ),
-        )
-    },
-)
-
-prior = Composite(
+prior = CombinePrior(
     [
-        Mc_prior,
-        q_prior,
-        s1_prior,
-        s2_prior,
+        m_1_prior,
+        m_2_prior,
+        theta_jn_prior,
+        phi_jl_prior,
+        theta_1_prior,
+        theta_2_prior,
+        phi_12_prior,
+        a_1_prior,
+        a_2_prior,
         dL_prior,
         t_c_prior,
         phase_c_prior,
-        cos_iota_prior,
         psi_prior,
         ra_prior,
-        sin_dec_prior,
-    ],
+        dec_prior,
+    ]
 )
 
-epsilon = 1e-3
-bounds = jnp.array(
-    [
-        [10.0, 80.0],
-        [0.125, 1.0],
-        [0, jnp.pi],
-        [0, 2 * jnp.pi],
-        [0.0, 1.0],
-        [0, jnp.pi],
-        [0, 2 * jnp.pi],
-        [0.0, 1.0],
-        [0.0, 2000],
-        [-0.05, 0.05],
-        [0.0, 2 * jnp.pi],
-        [-1.0, 1.0],
-        [0.0, jnp.pi],
-        [0.0, 2 * jnp.pi],
-        [-1.0, 1.0],
-    ]
-) + jnp.array([[epsilon, -epsilon]])
+sample_transforms = [
+    # all the user reparametrization transform
+    ComponentMassesToChirpMassMassRatioTransform(name_mapping=[["m_1", "m_2"], ["M_c", "q"]]),
+    DistanceToSNRWeightedDistanceTransform(name_mapping=[["d_L"], ["d_hat_unbounded"]], conditional_names=["M_c","ra", "dec", "psi", "iota"], gps_time=gps, ifos=ifos, dL_min=dL_prior.xmin, dL_max=dL_prior.xmax),
+    GeocentricArrivalPhaseToDetectorArrivalPhaseTransform(name_mapping = [["phase_c"], ["phase_det"]], conditional_names=["ra", "dec", "psi", "iota"], gps_time=gps, ifo=ifos[0]),
+    GeocentricArrivalTimeToDetectorArrivalTimeTransform(name_mapping = [["t_c"], ["t_det_unbounded"]], tc_min=t_c_prior.xmin, tc_max=t_c_prior.xmax, conditional_names=["ra", "dec"], gps_time=gps, ifo=ifos[0]),
+    SkyFrameToDetectorFrameSkyPositionTransform(name_mapping = [["ra", "dec"], ["zenith", "azimuth"]], gps_time=gps, ifos=ifos),
+    # all the bound to unbound transform
+    BoundToUnbound(name_mapping = [["M_c"], ["M_c_unbounded"]], original_lower_bound=10.0, original_upper_bound=80.0),
+    BoundToUnbound(name_mapping = [["iota"], ["iota_unbounded"]], original_lower_bound=0., original_upper_bound=jnp.pi),
+    BoundToUnbound(name_mapping = [["psi"], ["psi_unbounded"]], original_lower_bound=0.0, original_upper_bound=jnp.pi),
+    BoundToUnbound(name_mapping = [["zenith"], ["zenith_unbounded"]], original_lower_bound=0.0, original_upper_bound=jnp.pi),
+    BoundToUnbound(name_mapping = [["azimuth"], ["azimuth_unbounded"]], original_lower_bound=0.0, original_upper_bound=2 * jnp.pi),
+    BoundToUnbound(name_mapping = [["phase_det"], ["phase_det_unbounded"]], original_lower_bound=0.0, original_upper_bound=2 * jnp.pi),
+    BoundToUnbound(name_mapping = [["q"], ["q_unbounded"]], original_lower_bound=0.125, original_upper_bound=1.),
+    BoundToUnbound(name_mapping = [["theta_jn"], ["theta_jn_unbounded"]] , original_lower_bound=0.0, original_upper_bound=jnp.pi),
+    BoundToUnbound(name_mapping = [["phi_jl"], ["phi_jl_unbounded"]] , original_lower_bound=0.0, original_upper_bound=2 * jnp.pi),
+    BoundToUnbound(name_mapping = [["theta_1"], ["theta_1_unbounded"]] , original_lower_bound=0.0, original_upper_bound=jnp.pi),
+    BoundToUnbound(name_mapping = [["theta_2"], ["theta_2_unbounded"]] , original_lower_bound=0.0, original_upper_bound=jnp.pi),
+    BoundToUnbound(name_mapping = [["phi_12"], ["phi_12_unbounded"]] , original_lower_bound=0.0, original_upper_bound=2 * jnp.pi),
+    BoundToUnbound(name_mapping = [["a_1"], ["a_1_unbounded"]] , original_lower_bound=0.0, original_upper_bound=1.0),
+    BoundToUnbound(name_mapping = [["a_2"], ["a_2_unbounded"]] , original_lower_bound=0.0, original_upper_bound=1.0),
+]
+
+likelihood_transforms = [
+    ComponentMassesToChirpMassMassRatioTransform(name_mapping=[["m_1", "m_2"], ["M_c", "q"]]),
+    SpinToCartesianSpinTransform(name_mapping=[["theta_jn", "phi_jl", "theta_1", "theta_2", "phi_12", "a_1", "a_2"], ["iota", "s1_x", "s1_y", "s1_z", "s2_x", "s2_y", "s2_z"]], freq_ref=f_ref),
+    MassRatioToSymmetricMassRatioTransform(name_mapping=[["q"], ["eta"]]),
+]
 
 likelihood = TransientLikelihoodFD(
-    [H1, L1], waveform=waveform, trigger_time=gps, duration=4, post_trigger_duration=2
+    ifos,
+    waveform=RippleIMRPhenomPv2(f_ref=f_ref),
+    trigger_time=gps,
+    duration=4,
+    post_trigger_duration=2,
 )
-# likelihood = HeterodynedTransientLikelihoodFD([H1, L1], prior=prior, bounds=bounds, waveform=waveform, trigger_time=gps, duration=4, post_trigger_duration=2)
 
 
-mass_matrix = jnp.eye(prior.n_dim)
+mass_matrix = jnp.eye(15)
 mass_matrix = mass_matrix.at[1, 1].set(1e-3)
 mass_matrix = mass_matrix.at[9, 9].set(1e-3)
 local_sampler_arg = {"step_size": mass_matrix * 1e-3}
 
-Adam_optimizer = optimization_Adam(n_steps=3000, learning_rate=0.01, noise_level=1, bounds=bounds)
+Adam_optimizer = optimization_Adam(n_steps=3000, learning_rate=0.01, noise_level=1)
 
-import optax
-n_epochs = 20
+n_epochs = 30
 n_loop_training = 100
-total_epochs = n_epochs * n_loop_training
-start = total_epochs//10
-learning_rate = optax.polynomial_schedule(
-    1e-3, 1e-4, 4.0, total_epochs - start, transition_begin=start
-)
+learning_rate = 1e-4
+
 
 jim = Jim(
     likelihood,
     prior,
+    sample_transforms=sample_transforms,
+    likelihood_transforms=likelihood_transforms,
     n_loop_training=n_loop_training,
     n_loop_production=20,
     n_local_steps=10,
@@ -148,18 +139,37 @@ jim = Jim(
     n_epochs=n_epochs,
     learning_rate=learning_rate,
     n_max_examples=30000,
-    n_flow_sample=100000,
+    n_flow_samples=100000,
     momentum=0.9,
     batch_size=30000,
     use_global=True,
-    keep_quantile=0.0,
     train_thinning=1,
     output_thinning=10,
     local_sampler_arg=local_sampler_arg,
-    # strategies=[Adam_optimizer,"default"],
+    strategies=[Adam_optimizer, "default"],
 )
 
-import numpy as np
-# chains = np.load('./GW150914_init.npz')['chain']
+jim.sample(jax.random.PRNGKey(42))
+jim.get_samples()
+jim.print_summary()
 
-jim.sample(jax.random.PRNGKey(42))#,initial_guess=chains)
+###########################################
+########## Visualize the Data #############
+###########################################
+import corner
+import matplotlib.pyplot as plt
+import numpy as np
+
+production_summary = jim.sampler.get_sampler_state(training=False)
+production_chain = production_summary["chains"].reshape(-1, len(jim.parameter_names)).T
+if jim.sample_transforms:
+    transformed_chain = jim.add_name(production_chain)
+    for transform in reversed(jim.sample_transforms):
+        transformed_chain = transform.backward(transformed_chain)
+result = transformed_chain
+labels = list(transformed_chain.keys())
+
+samples = np.array(list(result.values())).reshape(int(len(labels)), -1) # flatten the array
+transposed_array = samples.T # transpose the array
+figure = corner.corner(transposed_array, labels=labels, plot_datapoints=False, title_quantiles=[0.16, 0.5, 0.84], show_titles=True, title_fmt='g', use_math_text=True)
+plt.savefig("GW1500914_Pv2_testing_reparam.jpeg")
