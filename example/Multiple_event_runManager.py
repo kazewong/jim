3ddc1fc593711cb82ecf54d1171289b779ad470d
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-08-31 10:19:41 +0800

    Fixed typo in Multiple_event_runManager.py

--- example/Multiple_event_runManager.py
+++ example/Multiple_event_runManager.py
@@ -1,11 +1,11 @@
 import jax
 import jax.numpy as jnp
 
-from jimgw.single_event.runManager import MultipleEventRunManager
+from jimgw.single_event.runManager import MultipleEventPERunManager
 
 jax.config.update("jax_enable_x64", True)
 
-run_manager = MultipleEventRunManager(
+run_manager = MultipleEventPERunManager(
     run_config_path="config",
 )
 
