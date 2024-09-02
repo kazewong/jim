c906c674d76fb8d3586d0c9d9a0dc076e682353b
xuyuon <116078673+xuyuon@users.noreply.github.com>
2024-09-02 10:45:36 +0800

    Added documentation in Multiple_event_runManager.py

--- example/Multiple_event_runManager.py
+++ example/Multiple_event_runManager.py
@@ -6,7 +6,7 @@ from jimgw.single_event.runManager import MultipleEventPERunManager
 jax.config.update("jax_enable_x64", True)
 
 run_manager = MultipleEventPERunManager(
-    run_config_path="config",
+    run_config_path="config", # the configuration file is stored in the config folder
 )
 
 run_manager.run()
