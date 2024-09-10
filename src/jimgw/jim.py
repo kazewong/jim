93b648317074bf07b54dd8ca1fa9d9d2d54a734e
kazewong <kazewong.physics@gmail.com>
2024-09-10 15:30:42 -0400

    Updating initial position sampling. Also changing GW150914_IMRPhenomD.py

--- src/jimgw/jim.py
+++ src/jimgw/jim.py
@@ -104,18 +104,19 @@ class Jim(object):
 
     def sample(self, key: PRNGKeyArray, initial_position: Array = jnp.array([])):
         if initial_position.size == 0:
-            initial_guess = []
-            for _ in range(self.sampler.n_chains):
-                flag = True
-                while flag:
-                    key = jax.random.split(key)[1]
-                    guess = self.prior.sample(key, 1)
-                    for transform in self.sample_transforms:
-                        guess = transform.forward(guess)
-                    guess = jnp.array([i for i in guess.values()]).T[0]
-                    flag = not jnp.all(jnp.isfinite(guess))
-                initial_guess.append(guess)
-            initial_position = jnp.array(initial_guess)
+            initial_position = jnp.zeros((self.sampler.n_chains, self.prior.n_dim)) + jnp.nan
+        
+            while not jax.tree.reduce(jnp.logical_and, jax.tree.map(lambda x: jnp.isfinite(x), initial_position)).all():
+                non_finite_index = jnp.any(~jax.tree.reduce(jnp.logical_and, jax.tree.map(lambda x: jnp.isfinite(x), initial_position)),axis=1)
+
+                key, subkey = jax.random.split(key)
+                guess = self.prior.sample(subkey, self.sampler.n_chains)
+                for transform in self.sample_transforms:
+                    guess = jax.vmap(transform.forward)(guess)
+                guess = jnp.array(jax.tree.leaves({key: guess[key] for key in self.parameter_names})).T
+                finite_guess = jnp.where(jnp.all(jax.tree.map(lambda x: jnp.isfinite(x), guess),axis=1))[0]
+                common_length = min(len(finite_guess), len(non_finite_index))
+                initial_position = initial_position.at[non_finite_index[:common_length]].set(guess[:common_length])
         self.sampler.sample(initial_position, None)  # type: ignore
 
     def maximize_likelihood(
