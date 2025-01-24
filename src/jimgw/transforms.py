623301185ef5c3364e33e5efdfa38f42dbd57648
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-01-24 14:41:09 +0800

    Add periodic BC transform

--- src/jimgw/transforms.py
+++ src/jimgw/transforms.py
@@ -532,3 +532,39 @@ def reverse_bijective_transform(
     reversed_transform.__repr__ = lambda: f"Reversed{repr(original_transform)}"
 
     return reversed_transform
+
+
+@jaxtyped(typechecker=typechecker)
+class BoundToPeriodic(BijectiveTransform):
+    """
+    Bound to unbound transformation
+    """
+
+    original_lower_bound: Float
+    original_upper_bound: Float
+
+    def __init__(
+        self,
+        name_mapping: tuple[list[str], list[str]],
+        original_lower_bound: Float,
+        original_upper_bound: Float,
+    ):
+
+        def logit(x):
+            return jnp.log(x / (1 - x))
+
+        super().__init__(name_mapping)
+        self.original_lower_bound = jnp.atleast_1d(original_lower_bound)
+        self.original_upper_bound = jnp.atleast_1d(original_upper_bound)
+
+        self.transform_func = lambda x: {
+            name_mapping[1][i]: jnp.mod(
+                x[name_mapping[0][i]] - self.original_lower_bound[i],
+                self.original_upper_bound[i] - self.original_lower_bound[i],
+            )
+            for i in range(len(name_mapping[0]))
+        }
+        self.inverse_transform_func = lambda x: {
+            name_mapping[0][i]: x[name_mapping[1][i]]
+            for i in range(len(name_mapping[1]))
+        }
