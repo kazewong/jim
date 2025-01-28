e946425bab0e592c46156313ec1d71afb11e2644
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-01-29 03:23:45 +0800

    Update test script

--- test/integration/test_periodic_uniform.py
+++ test/integration/test_periodic_uniform.py
@@ -6,20 +6,23 @@ import jax
 import jax.numpy as jnp
 
 from jimgw.jim import Jim
-from jimgw.prior import UniformPeriodicPrior
-from jimgw.transforms import PeriodicTransform
+from jimgw.prior import UniformPeriodicPrior, UniformPrior
+from jimgw.transforms import PeriodicTransform, BoundToUnbound
 from jimgw.base import LikelihoodBase
 from jaxtyping import Float
 
 jax.config.update("jax_enable_x64", True)
 
-test_prior = UniformPeriodicPrior(0.0, 2.0 * jnp.pi, ["test"])
-
-prior = test_prior
-
+prior = UniformPeriodicPrior(0.0, 2.0 * jnp.pi, ["test"])
 sample_transforms = [
     PeriodicTransform("test", 0.0, 2.0 * jnp.pi)
 ]
+
+# Uncomment the following code to use a uniform prior without periodicity
+# prior = UniformPrior(0.0, 2.0 * jnp.pi, ["test"])
+# sample_transforms = [
+#     BoundToUnbound(name_mapping = [["test"], ["test_unbound"]], original_lower_bound=0.0, original_upper_bound=2 * jnp.pi)
+# ]
 
 likelihood_transforms = []
 
@@ -33,9 +36,9 @@ class CosLikelihood(LikelihoodBase):
 
 likelihood = CosLikelihood()
 
-mass_matrix = jnp.eye(2)
+mass_matrix = jnp.eye(prior.n_dim)
 
-n_local_steps=500
+n_local_steps=1_000
 
 jim = Jim(
     likelihood,
@@ -63,9 +66,23 @@ jim.sample(jax.random.PRNGKey(42))
 jim.print_summary()
 samples = jim.get_samples()
 
+# Uncomment the following code to plot the walker history and histogram
 # import matplotlib.pyplot as plt
 
 # plt.plot(samples["test"])
 # plt.ylim(0.0, 2.0 * jnp.pi)
-# plt.xlim(0, n_local_steps)
-# plt.savefig("test_periodic_uniform.jpg")
+# plt.xlim(0, 300)
+# if prior.n_dim == 1:
+#     plt.savefig("walker_history_uniform.jpg")
+# else:
+#     plt.savefig("walker_history_periodic.jpg")
+# plt.close()
+
+# plt.hist(samples["test"])
+# plt.ylim(0.0)
+# plt.xlim(0.0, 2.0 * jnp.pi)
+# if prior.n_dim == 1:
+#     plt.savefig("histogram_uniform.jpg")
+# else:
+#     plt.savefig("histogram_periodic.jpg")
+# plt.close()
