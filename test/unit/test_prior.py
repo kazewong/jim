bb4550a4107b3fda721f5c5a2df0336fc7baea2b
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-02-14 17:09:31 +0800

    Formatting

--- test/unit/test_prior.py
+++ test/unit/test_prior.py
@@ -1,9 +1,19 @@
 import jax
-jax.config.update("jax_enable_x64", True)
+import jax.numpy as jnp
 
-from jimgw.prior import *
+from jimgw.prior import (
+    LogisticDistribution,
+    StandardNormalDistribution,
+    UniformPrior,
+    SinePrior,
+    CosinePrior,
+    UniformSpherePrior,
+    PowerLawPrior,
+)
 from jimgw.utils import trace_prior_parent
 import scipy.stats as stats
+
+jax.config.update("jax_enable_x64", True)
 
 
 class TestUnivariatePrior:
@@ -17,7 +27,9 @@ class TestUnivariatePrior:
 
         # Cross-check log_prob with scipy.stats.logistic
         x = jnp.linspace(-10.0, 10.0, 1000)
-        assert jnp.allclose(jax.vmap(p.log_prob)(p.add_name(x[None])), stats.logistic.logpdf(x))
+        assert jnp.allclose(
+            jax.vmap(p.log_prob)(p.add_name(x[None])), stats.logistic.logpdf(x)
+        )
 
     def test_standard_normal(self):
         p = StandardNormalDistribution(["x"])
@@ -29,7 +41,9 @@ class TestUnivariatePrior:
 
         # Cross-check log_prob with scipy.stats.norm
         x = jnp.linspace(-10.0, 10.0, 1000)
-        assert jnp.allclose(jax.vmap(p.log_prob)(p.add_name(x[None])), stats.norm.logpdf(x))
+        assert jnp.allclose(
+            jax.vmap(p.log_prob)(p.add_name(x[None])), stats.norm.logpdf(x)
+        )
 
     def test_uniform(self):
         xmin, xmax = -10.0, 10.0
@@ -37,7 +51,7 @@ class TestUnivariatePrior:
 
         # Check that all the samples are finite
         samples = p.sample(jax.random.PRNGKey(0), 10000)
-        assert jnp.all(jnp.isfinite(samples['x']))
+        assert jnp.all(jnp.isfinite(samples["x"]))
 
         # Check that the log_prob are finite
         log_prob = jax.vmap(p.log_prob)(samples)
@@ -53,7 +67,7 @@ class TestUnivariatePrior:
 
         # Check that all the samples are finite
         samples = p.sample(jax.random.PRNGKey(0), 10000)
-        assert jnp.all(jnp.isfinite(samples['x']))
+        assert jnp.all(jnp.isfinite(samples["x"]))
 
         # Check that the log_prob are finite
         log_prob = jax.vmap(p.log_prob)(samples)
@@ -64,14 +78,14 @@ class TestUnivariatePrior:
         y = jax.vmap(p.base_prior.base_prior.transform)(x)
         y = jax.vmap(p.base_prior.transform)(y)
         y = jax.vmap(p.transform)(y)
-        assert jnp.allclose(jax.vmap(p.log_prob)(y), jnp.log(jnp.sin(y['x'])/2.0))
-        
+        assert jnp.allclose(jax.vmap(p.log_prob)(y), jnp.log(jnp.sin(y["x"]) / 2.0))
+
     def test_cosine(self):
         p = CosinePrior(["x"])
 
         # Check that all the samples are finite
         samples = p.sample(jax.random.PRNGKey(0), 10000)
-        assert jnp.all(jnp.isfinite(samples['x']))
+        assert jnp.all(jnp.isfinite(samples["x"]))
 
         # Check that the log_prob are finite
         log_prob = jax.vmap(p.log_prob)(samples)
@@ -81,16 +95,16 @@ class TestUnivariatePrior:
         x = trace_prior_parent(p, [])[0].add_name(jnp.linspace(-10.0, 10.0, 1000)[None])
         y = jax.vmap(p.base_prior.transform)(x)
         y = jax.vmap(p.transform)(y)
-        assert jnp.allclose(jax.vmap(p.log_prob)(y), jnp.log(jnp.cos(y['x'])/2.0))
+        assert jnp.allclose(jax.vmap(p.log_prob)(y), jnp.log(jnp.cos(y["x"]) / 2.0))
 
     def test_uniform_sphere(self):
         p = UniformSpherePrior(["x"])
 
         # Check that all the samples are finite
         samples = p.sample(jax.random.PRNGKey(0), 10000)
-        assert jnp.all(jnp.isfinite(samples['x_mag']))
-        assert jnp.all(jnp.isfinite(samples['x_theta']))
-        assert jnp.all(jnp.isfinite(samples['x_phi']))
+        assert jnp.all(jnp.isfinite(samples["x_mag"]))
+        assert jnp.all(jnp.isfinite(samples["x_theta"]))
+        assert jnp.all(jnp.isfinite(samples["x_phi"]))
 
         # Check that the log_prob are finite
         log_prob = jax.vmap(p.log_prob)(samples)
@@ -104,18 +118,33 @@ class TestUnivariatePrior:
 
             # Check that all the samples are finite
             samples = p.sample(jax.random.PRNGKey(0), 10000)
-            assert jnp.all(jnp.isfinite(samples['x']))
+            assert jnp.all(jnp.isfinite(samples["x"]))
 
             # Check that the log_prob are finite
             log_prob = jax.vmap(p.log_prob)(samples)
             assert jnp.all(jnp.isfinite(log_prob))
 
             # Check that the log_prob are correct in the support
-            x = trace_prior_parent(p, [])[0].add_name(jnp.linspace(-10.0, 10.0, 1000)[None])
+            x = trace_prior_parent(p, [])[0].add_name(
+                jnp.linspace(-10.0, 10.0, 1000)[None]
+            )
             y = jax.vmap(p.transform)(x)
             if alpha < -1.0:
-                assert jnp.allclose(jax.vmap(p.log_prob)(y), alpha * jnp.log(y['x']) + jnp.log(-alpha-1) - jnp.log(xmin**(alpha+1)-xmax**(alpha+1)))
+                assert jnp.allclose(
+                    jax.vmap(p.log_prob)(y),
+                    alpha * jnp.log(y["x"])
+                    + jnp.log(-alpha - 1)
+                    - jnp.log(xmin ** (alpha + 1) - xmax ** (alpha + 1)),
+                )
             elif alpha > -1.0:
-                assert jnp.allclose(jax.vmap(p.log_prob)(y), alpha * jnp.log(y['x']) + jnp.log(alpha+1) - jnp.log(xmax**(alpha+1)-xmin**(alpha+1)))
+                assert jnp.allclose(
+                    jax.vmap(p.log_prob)(y),
+                    alpha * jnp.log(y["x"])
+                    + jnp.log(alpha + 1)
+                    - jnp.log(xmax ** (alpha + 1) - xmin ** (alpha + 1)),
+                )
             else:
-                assert jnp.allclose(jax.vmap(p.log_prob)(y), -jnp.log(y['x'])-jnp.log(jnp.log(xmax)-jnp.log(xmin)))
+                assert jnp.allclose(
+                    jax.vmap(p.log_prob)(y),
+                    -jnp.log(y["x"]) - jnp.log(jnp.log(xmax) - jnp.log(xmin)),
+                )
