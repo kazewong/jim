acb52a8e9f360bb2f777022dda962d83836dd9c7
Samson Leong (wkstn) <55839002+SSL32081@users.noreply.github.com>
2025-04-09 22:42:00 +0800

    Conform parameter names in generate bilby spins with names in transform functions

--- test/unit/source_files/bilby_spin.py
+++ test/unit/source_files/bilby_spin.py
@@ -16,28 +16,28 @@ fRefs = [10, 30, 50]
 for fRef in fRefs:
     m1, m2 = uniform(1, 70, (2, N_samples))
     input_dict = {
-        'thetaJN': uniform(0, np.pi, N_samples),
-        'phiJL': uniform(0, 2 * np.pi, N_samples),
-        'theta1': uniform(0, np.pi, N_samples),
-        'theta2': uniform(0, np.pi, N_samples),
-        'phi12': uniform(0, 2 * np.pi, N_samples),
-        'chi1': uniform(0, 1, N_samples),
-        'chi2': uniform(0, 1, N_samples),
-        'm1': np.where(m1 >= m2, m1, m2),
-        'm2': np.where(m1 >= m2, m2, m1),
+        'theta_jn': uniform(0, np.pi, N_samples),
+        'phi_jl': uniform(0, 2 * np.pi, N_samples),
+        'tilt_1': uniform(0, np.pi, N_samples),
+        'tilt_2': uniform(0, np.pi, N_samples),
+        'phi_12': uniform(0, 2 * np.pi, N_samples),
+        'a_1': uniform(0, 1, N_samples),
+        'a_2': uniform(0, 1, N_samples),
+        'm_1': np.where(m1 >= m2, m1, m2),
+        'm_2': np.where(m1 >= m2, m2, m1),
         'fRef': np.ones(N_samples) * fRef,
-        'phiRef': uniform(0, 2 * np.pi, N_samples),
+        'phase_c': uniform(0, 2 * np.pi, N_samples),
     }
 
-    assert np.all(input_dict['m1'] >= input_dict['m2'])
+    assert np.all(input_dict['m_1'] >= input_dict['m_2'])
 
     np.savez(
         f"{outdir}/spin_angles_input/spin_angles_input_fRef_{fRef}.npz",
         **input_dict
     )
 
-    input_dict['m1'] *= MSUN_SI
-    input_dict['m2'] *= MSUN_SI
+    input_dict['m_1'] *= MSUN_SI
+    input_dict['m_2'] *= MSUN_SI
 
     input_array = np.array(list(input_dict.values())).T
 
@@ -50,13 +50,13 @@ for fRef in fRefs:
 
     np.savez(
         f"{outdir}/cartesian_spins_output_for_bilby/cartesian_spins_output_for_bilby_fRef_{fRef}.npz",
-        iota=bilby_outputs[0],
-        S1x = bilby_outputs[1],
-        S1y = bilby_outputs[2],
-        S1z = bilby_outputs[3],
-        S2x = bilby_outputs[4],
-        S2y = bilby_outputs[5],
-        S2z = bilby_outputs[6]
+        iota = bilby_outputs[0],
+        s1_x = bilby_outputs[1],
+        s1_y = bilby_outputs[2],
+        s1_z = bilby_outputs[3],
+        s2_x = bilby_outputs[4],
+        s2_y = bilby_outputs[5],
+        s2_z = bilby_outputs[6]
     )
 
 # The following script generates cartesian angles input files and the spin angles outputs for lal
@@ -70,18 +70,18 @@ for fRef in fRefs:
 
     input_dict = {
         'iota': uniform(0, np.pi, N_samples),
-        'S1x': S1[0],
-        'S1y': S1[1],
-        'S1z': S1[2],
-        'S2x': S2[0],
-        'S2y': S2[1],
-        'S2z': S2[2],
-        'm1': np.where(m1 >= m2, m1, m2),
-        'm2': np.where(m1 >= m2, m2, m1),
+        's1_x': S1[0],
+        's1_y': S1[1],
+        's1_z': S1[2],
+        's2_x': S2[0],
+        's2_y': S2[1],
+        's2_z': S2[2],
+        'm_1': np.where(m1 >= m2, m1, m2),
+        'm_2': np.where(m1 >= m2, m2, m1),
         'fRef': np.ones(N_samples) * fRef,
-        'phiRef': uniform(0, 2*np.pi, N_samples)
+        'phase_c': uniform(0, 2*np.pi, N_samples)
     }
-    assert np.all(input_dict['m1'] >= input_dict['m2'])
+    assert np.all(input_dict['m_1'] >= input_dict['m_2'])
 
     np.savez(
         f"{outdir}/cartesian_spins_input/cartesian_spins_input_fRef_{fRef}.npz",
@@ -97,11 +97,11 @@ for fRef in fRefs:
 
     np.savez(
         f"{outdir}/spin_angles_output_for_bilby/spin_angles_output_for_bilby_fRef_{fRef}.npz",
-        thetaJN = bilby_outputs[0],
-        phiJL = bilby_outputs[1],
-        theta1 = bilby_outputs[2],
-        theta2 = bilby_outputs[3],
-        phi12 = bilby_outputs[4],
-        chi1 = bilby_outputs[5],
-        chi2 = bilby_outputs[6],
+        theta_jn = bilby_outputs[0],
+        phi_jl = bilby_outputs[1],
+        tilt_1 = bilby_outputs[2],
+        tilt_2 = bilby_outputs[3],
+        phi_12 = bilby_outputs[4],
+        a_1 = bilby_outputs[5],
+        a_2 = bilby_outputs[6],
     )
