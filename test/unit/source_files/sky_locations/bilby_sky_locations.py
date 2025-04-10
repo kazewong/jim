2241dd3d8d5d28d9b5f979f14306fab3bc0c0fc0
CharmaineWONG2 <wongsheeman0208@gmail.com>
2025-04-10 16:30:36 +0800

    add scripts for generating sky locations in bilby

---
+++ test/unit/source_files/sky_locations/bilby_sky_locations.py
@@ -0,0 +1,44 @@
+import bilby
+from bilby.gw.utils import zenith_azimuth_to_ra_dec
+from itertools import combinations
+import jax
+import jax.numpy as jnp
+
+jax.config.update("jax_enable_x64", True)
+
+outdir = "test/unit/source_files/"
+N_samples = 100
+ifo_names = ["H1", "L1", "V1"]
+geocent_time = [1126259642.413]
+
+
+for ifo_pair in combinations(ifo_names, 2):
+    ifo_pair = list(ifo_pair)
+    ifos = bilby.gw.detector.InterferometerList(ifo_pair)
+    for time in geocent_time:
+        key = jax.random.PRNGKey(42)
+        key, subkey = jax.random.split(key)
+        subkeys = jax.random.split(subkey, 2)
+        zenith = jax.random.uniform(subkeys[0], (N_samples,), minval=0, maxval=jnp.pi)
+        azimuth = jax.random.uniform(
+            subkeys[1], (N_samples,), minval=0, maxval=2 * jnp.pi
+        )
+        input = jnp.array([zenith, azimuth]).T
+        output = []
+        for row in input:
+            output.append(zenith_azimuth_to_ra_dec(*row, time, ifos))
+        output = jnp.array(output).T
+        ra, dec = output
+        input_dict = {
+            "zenith": zenith,
+            "azimuth": azimuth,
+            "ra": ra,
+            "dec": dec,
+            "gps_time": time,
+            "ifo_pair": ifo_pair,
+        }
+
+        jnp.savez(
+            f"{outdir}/sky_locations/test_{ifo_pair[0]}_{ifo_pair[1]}_{time}.npz",
+            **input_dict,
+        )
