f8194095eeb0a2d38f718a31d9cb62072d1969ca
Kaze Wong <kazewong.physics@gmail.com>
2025-04-18 19:59:16 -0400

    update rng handling

--- src/jimgw/jim.py
+++ src/jimgw/jim.py
@@ -4,7 +4,6 @@ from flowMC.resource_strategy_bundle.RQSpline_MALA_PT import RQSpline_MALA_PT_Bu
 from flowMC.resource.buffers import Buffer
 from flowMC.Sampler import Sampler
 from jaxtyping import Array, Float, PRNGKeyArray
-from typing import Optional
 
 from jimgw.base import LikelihoodBase
 from jimgw.prior import Prior
@@ -78,8 +77,10 @@ class Jim(object):
         if rng_key is jax.random.PRNGKey(0):
             print("No rng_key provided. Using default key with seed=0.")
 
+        rng_key, subkey = jax.random.split(rng_key)
+
         resource_strategy_bundle = RQSpline_MALA_PT_Bundle(
-            rng_key=rng_key,
+            rng_key=subkey,
             n_chains=n_chains,
             n_dims=self.prior.n_dim,
             logpdf=self.posterior,
@@ -147,7 +148,6 @@ class Jim(object):
 
     def sample(
         self,
-        key: Optional[PRNGKeyArray] = None,
         initial_position: Array = jnp.array([]),
     ):
         if initial_position.size == 0:
@@ -155,9 +155,7 @@ class Jim(object):
                 jnp.zeros((self.sampler.n_chains, self.prior.n_dim)) + jnp.nan
             )
 
-            if key is not None:
-                print("Provided key will override the existing sampler RNG key")
-                key, self.sampler.rng_key = jax.random.split(key)
+            rng_key, subkey = jax.random.split(self.sampler.rng_key)
 
             while not jax.tree.reduce(
                 jnp.logical_and,
@@ -173,7 +171,7 @@ class Jim(object):
                     )
                 )[0]
 
-                key, subkey = jax.random.split(key)
+                rng_key, subkey = jax.random.split(subkey)
                 guess = self.prior.sample(subkey, self.sampler.n_chains)
                 for transform in self.sample_transforms:
                     guess = jax.vmap(transform.forward)(guess)
@@ -187,66 +185,8 @@ class Jim(object):
                 initial_position = initial_position.at[
                     non_finite_index[:common_length]
                 ].set(guess[:common_length])
+            self.sampler.rng_key = rng_key
         self.sampler.sample(initial_position, {})  # type: ignore
-
-    def print_summary(self, transform: bool = True):
-        """
-        Generate summary of the run
-
-        """
-
-        train_summary = self.sampler.get_sampler_state(training=True)
-        production_summary = self.sampler.get_sampler_state(training=False)
-
-        training_chain = train_summary["chains"].reshape(-1, self.prior.n_dim).T
-        training_chain = self.add_name(training_chain)
-        if transform:
-            for sample_transform in reversed(self.sample_transforms):
-                training_chain = jax.vmap(sample_transform.backward)(training_chain)
-        training_log_prob = train_summary["log_prob"]
-        training_local_acceptance = train_summary["local_accs"]
-        training_global_acceptance = train_summary["global_accs"]
-        training_loss = train_summary["loss_vals"]
-
-        production_chain = production_summary["chains"].reshape(-1, self.prior.n_dim).T
-        production_chain = self.add_name(production_chain)
-        if transform:
-            for sample_transform in reversed(self.sample_transforms):
-                production_chain = jax.vmap(sample_transform.backward)(production_chain)
-        production_log_prob = production_summary["log_prob"]
-        production_local_acceptance = production_summary["local_accs"]
-        production_global_acceptance = production_summary["global_accs"]
-
-        print("Training summary")
-        print("=" * 10)
-        for key, value in training_chain.items():
-            print(f"{key}: {value.mean():.3f} +/- {value.std():.3f}")
-        print(
-            f"Log probability: {training_log_prob.mean():.3f} +/- {training_log_prob.std():.3f}"
-        )
-        print(
-            f"Local acceptance: {training_local_acceptance.mean():.3f} +/- {training_local_acceptance.std():.3f}"
-        )
-        print(
-            f"Global acceptance: {training_global_acceptance.mean():.3f} +/- {training_global_acceptance.std():.3f}"
-        )
-        print(
-            f"Max loss: {training_loss.max():.3f}, Min loss: {training_loss.min():.3f}"
-        )
-
-        print("Production summary")
-        print("=" * 10)
-        for key, value in production_chain.items():
-            print(f"{key}: {value.mean():.3f} +/- {value.std():.3f}")
-        print(
-            f"Log probability: {production_log_prob.mean():.3f} +/- {production_log_prob.std():.3f}"
-        )
-        print(
-            f"Local acceptance: {production_local_acceptance.mean():.3f} +/- {production_local_acceptance.std():.3f}"
-        )
-        print(
-            f"Global acceptance: {production_global_acceptance.mean():.3f} +/- {production_global_acceptance.std():.3f}"
-        )
 
     def get_samples(self, training: bool = False) -> dict:
         """
@@ -279,6 +219,3 @@ class Jim(object):
         for sample_transform in reversed(self.sample_transforms):
             chains = jax.vmap(sample_transform.backward)(chains)
         return chains
-
-    def plot(self):
-        pass
