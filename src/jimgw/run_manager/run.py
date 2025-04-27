c9b265386659c77d561085005c941507bf3362a4
kazewong <kazewong.physics@gmail.com>
2025-04-27 11:43:22 -0400

    Define run base class

---
+++ src/jimgw/run_manager/run.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Run(ABC):
+    """
+
+    A `Run` is a template of priors, likelihood transforms, and sample transforms.
+
+    It is aimed to be an abstraction which wrap the flexible but complicated APIs of core jim into an object that the users only interact with the underlying `jim` through the parameters defined in the Run.
+
+    The most important property of a Run instance is it needs to be able to deterministically declared. All arguments to a run has to be explicitly provided, and the content of a Run should be exactly the same given the same arguments.
+    """
+
+    @abstractmethod
+    def serialize(self, path: str = "./"):
+        raise NotImplementedError
+
+    @abstractmethod
+    def deserialize(self, path: str):
+        raise NotImplementedError
