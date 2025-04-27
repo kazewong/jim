61646ca53cfda0c968f244b6e6fc0d8e2df71cdd
kazewong <kazewong.physics@gmail.com>
2025-04-27 12:01:30 -0400

    Update requirement in Run

--- src/jimgw/run_manager/run.py
+++ src/jimgw/run_manager/run.py
@@ -1,15 +1,23 @@
 from abc import ABC, abstractmethod
 from typing import Self
+from jimgw.core.base import LikelihoodBase
+from jimgw.core.prior import Prior
+from jimgw.core.transforms import BijectiveTransform, NtoMTransform
 
 class Run(ABC):
     """
 
     A `Run` is a template of priors, likelihood transforms, and sample transforms.
 
-    It is aimed to be an abstraction which wrap the flexible but complicated APIs of core jim into an object that the users only interact with the underlying `jim` through the parameters defined in the Run.
+    It is aimed to be an abstraction which wrap the flexible but complicated APIs of core jim into an object that the users only interact with the underlying `jim` through the parameters defined in the Run. It is responsible for constructing the likelihood object, the prior, sample_transform, and likelihood_transform needed in jim.
 
     The most important property of a Run instance is it needs to be able to deterministically declared. All arguments to a run has to be explicitly provided, and the content of a Run should be exactly the same given the same arguments.
     """
+
+    likelihood: LikelihoodBase
+    prior: Prior
+    sample_transforms: list[BijectiveTransform]
+    likelihood_transforms: list[NtoMTransform]
 
     @abstractmethod
     def serialize(self, path: str = "./"):
