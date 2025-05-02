5b73abb900cdb5d0f1d6c7ebd1e8a43f4eb71f08
kazewong <kazewong.physics@gmail.com>
2025-05-01 20:50:16 -0400

    Add skeleton definition in assets

--- src/jimgw/pipeline/assets.py
+++ src/jimgw/pipeline/assets.py
@@ -5,41 +5,126 @@ from jimgw.run_manager.single_event_run import SingleEventRun
 
 # Create asset group for run and configuration0
 
-@dg.asset
-def data_strain():
-    pass
 
-@dg.asset
-def psd():
-    pass
-
-@dg.asset
+@dg.asset(
+    group_name="prerun",
+    description="Configuration file for the run.",
+)
 def config_file():
     pass
 
-@dg.asset
-def imrphenom_pv2_standard_cbc_run():
+
+@dg.multi_asset(
+    specs=[
+        dg.AssetSpec("strain", deps=[config_file]),
+        dg.AssetSpec("psd", deps=[config_file]),
+    ],
+    group_name="prerun",
+)
+def raw_data():
+    """
+    This is a placeholder function for the raw data.
+    It is used to demonstrate how to create a Dagster asset.
+    """
+    pass
+
+
+@dg.multi_asset(
+    specs=[
+        dg.AssetSpec("training_chains", deps=[raw_data]),
+        dg.AssetSpec("training_log_prob", deps=[raw_data]),
+        dg.AssetSpec("training_local_acceptance", deps=[raw_data]),
+        dg.AssetSpec("training_global_acceptance", deps=[raw_data]),
+        dg.AssetSpec("training_loss", deps=[raw_data]),
+        dg.AssetSpec("production_chains", deps=[raw_data]),
+        dg.AssetSpec("production_log_prob", deps=[raw_data]),
+        dg.AssetSpec("production_local_acceptance", deps=[raw_data]),
+        dg.AssetSpec("production_global_acceptance", deps=[raw_data]),
+        dg.AssetSpec("auxiliary_nf_samples", deps=[raw_data]),
+        dg.AssetSpec("auxiliary_prior_samples", deps=[raw_data]),
+    ],
+    group_name="run",
+)
+def run():
     """
     This is a placeholder function for the IMRPhenomPv2StandardCBCRun class.
     It is used to demonstrate how to create a Dagster asset.
     """
     pass
 
+
 # Create asset group for diagnostics
 
-@dg.multi_asset(specs=[dg.AssetSpec("")])
 
-@dg.asset
+@dg.asset(group_name="diagnostics", deps=["training_loss"])
 def loss_plot():
     pass
 
-@dg.asset
-def sample_corner():
+
+@dg.asset(group_name="diagnostics", deps=["training_chains"])
+def training_chains_corner_plot():
     pass
 
-@dg.asset
-def nf_sample_corner():
+
+@dg.asset(group_name="diagnostics", deps=["training_chains"])
+def training_chains_trace_plot():
     pass
 
-@dg.asset
-def prior
+
+@dg.asset(group_name="diagnostics", deps=["training_chains"])
+def training_chains_rhat_plot():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["training_log_prob"])
+def training_log_prob_distribution():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["training_log_prob"])
+def training_log_prob_evolution():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["training_local_acceptance"])
+def training_local_acceptance_plot():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["training_global_acceptance"])
+def training_global_acceptance_plot():
+    pass
+
+@dg.asset(group_name="diagnostics", deps=["production_chains"])
+def production_chains_corner_plot():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["production_chains"])
+def production_chains_trace_plot():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["production_chains"])
+def production_chains_rhat_plot():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["production_log_prob"])
+def production_log_prob_distribution():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["production_log_prob"])
+def production_log_prob_evolution():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["production_local_acceptance"])
+def production_local_acceptance_plot():
+    pass
+
+
+@dg.asset(group_name="diagnostics", deps=["production_global_acceptance"])
+def production_global_acceptance_plot():
+    pass
