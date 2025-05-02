1aa6f2f5325cecf96cc512c93a9291417a1de11f
kazewong <kazewong.physics@gmail.com>
2025-05-02 05:59:37 -0400

    scaffold run_manager plotting utility functions

--- src/jimgw/run_manager/run_manager.py
+++ src/jimgw/run_manager/run_manager.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import corner
 import numpy as np
+import jax
 from jaxtyping import Float, Array
 from jimgw.core.jim import Jim
 from jimgw.run_manager.run import Run
 
 from flowMC.resource.buffers import Buffer
+from flowMC.resource.nf_model.base import NFModel
 import logging
-
 
 
 class RunManager:
@@ -57,9 +58,8 @@ class RunManager:
         use_math_text: bool = True,
     ):
         """
-        plot corner plot of the samples.
+        Plot corner plot of the samples.
         """
-
         samples = self.jim.get_samples(training=training)
         param_names = list(samples.keys())
         samples = np.array(list(samples.values())).reshape(int(len(param_names)), -1).T
@@ -76,13 +76,13 @@ class RunManager:
         plt.savefig(path)
         plt.close()
 
-    def plot_trace(self):
-        raise NotImplementedError
-
     def plot_loss(self):
+        """
+        Plot the loss history during training.
+        """
         assert isinstance(
             loss := self.jim.sampler.resources["loss_buffer"], Buffer
-        ), "Loss buffer is not a jax array"
+        ), "Loss buffer is not a Buffer"
         plt.figure(figsize=(10, 8))
         plt.title("NF loss")
         plt.plot(loss.data)
@@ -94,16 +94,97 @@ class RunManager:
         plt.close()
 
     def plot_nf_sample(self):
-        raise NotImplementedError
-
-    def serialize_run(self):
-        raise NotImplementedError
+        """
+        Plot samples from the normalizing flow to visualize the learned distribution.
+        """
+        assert isinstance(
+            nf_model := self.jim.sampler.resources["model"], NFModel
+        ), "NF model is not a normalizing flow model"
+        samples = nf_model.sample(
+            jax.random.PRNGKey(0), 10000
+        )
+        param_names = list(self.jim.get_samples().keys())
+        samples = np.array(samples).reshape(int(len(param_names)), -1).T
+        corner.corner(
+            samples,
+            labels=param_names,
+            plot_datapoints=True,
+            show_titles=True,
+            title_fmt=".2E",
+            use_math_text=True,
+        )
+        path = f"{self.result_dir}/nf_samples.jpeg"
+        plt.savefig(path)
+        plt.close()
 
     def plot_prior(self):
-        raise NotImplementedError
+        """
+        Plot samples from the prior distribution.
+        """
+        samples = self.jim.prior.sample(jax.random.PRNGKey(0), 10000)
+        param_names = list(samples.keys())
+        samples = np.array(list(samples.values())).reshape(int(len(param_names)), -1).T
+        corner.corner(
+            samples,
+            labels=param_names,
+            plot_datapoints=True,
+            show_titles=True,
+            title_fmt=".2E",
+            use_math_text=True,
+        )
+        path = f"{self.result_dir}/prior_samples.jpeg"
+        plt.savefig(path)
+        plt.close()
 
     def plot_acceptances(self):
-        raise NotImplementedError
+        """
+        Plot the local and global acceptance rates during sampling.
+        """
+        plt.figure(figsize=(10, 8))
+        plt.subplot(2, 1, 1)
+        plt.title("Local acceptance rate")
+        local_acc = np.array(self.jim.sampler.resources["local_accs_training"])
+        plt.plot(local_acc)
+        plt.xlabel("iteration")
+        plt.xlim(0, None)
+        plt.ylabel("acceptance rate")
 
-    def generate_summar(self):
-        raise NotImplementedError
+        plt.subplot(2, 1, 2)
+        plt.title("Global acceptance rate")
+        global_acc = np.array(self.jim.sampler.resources["global_accs_training"])
+        plt.plot(global_acc)
+        plt.xlabel("iteration")
+        plt.xlim(0, None)
+        plt.ylabel("acceptance rate")
+
+        path = f"{self.result_dir}/acceptance_rates.jpeg"
+        plt.savefig(path)
+        plt.close()
+
+    def generate_summary(self):
+        """
+        Generate a summary of the sampling results including statistics
+        and diagnostics about the chains.
+        """
+        samples = self.jim.get_samples()
+        param_names = list(samples.keys())
+        samples_array = (
+            np.array(list(samples.values())).reshape(int(len(param_names)), -1).T
+        )
+
+        summary = {}
+        for i, param in enumerate(param_names):
+            param_samples = samples_array[:, i]
+            quantiles = np.percentile(param_samples, [16, 50, 84])
+            summary[param] = {
+                "median": quantiles[1],
+                "lower_err": quantiles[1] - quantiles[0],
+                "upper_err": quantiles[2] - quantiles[1],
+                "mean": np.mean(param_samples),
+                "std": np.std(param_samples),
+            }
+
+        return summary
+
+    def serialize_run(self):
+        pass
