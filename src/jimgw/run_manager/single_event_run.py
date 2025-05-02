3b518e47ebd4bd266290ba41657e01a0eed468f8
kazewong <kazewong.physics@gmail.com>
2025-05-02 19:51:09 -0400

    add TestSingleEvenetRun

--- src/jimgw/run_manager/single_event_run.py
+++ src/jimgw/run_manager/single_event_run.py
@@ -1,5 +1,12 @@
 from jimgw.run_manager.run import Run
-from jimgw.core.single_event.detector import GroundBased2G
+from jimgw.core.single_event.detector import GroundBased2G, H1, L1, detector_preset
+from jimgw.core.single_event.likelihood import ZeroLikelihood
+from jimgw.core.prior import (
+    CombinePrior,
+    UniformPrior,
+)
+from typing import Sequence
+import yaml
 
 
 class SingleEventRun(Run):
@@ -17,5 +24,63 @@ class SingleEventRun(Run):
     post_trigger_length: int  # Length of segment after the trigger
     f_min: int  # Minimum frequency
     f_max: int  # Maximum frequency
-    ifos: list[GroundBased2G]  # Set of detectors
+    ifos: Sequence[GroundBased2G]  # Set of detectors
     f_ref: int  # Reference frequency
+
+
+class TestSingleEventRun(SingleEventRun):
+    """
+    A test run with zero likelihood
+    """
+
+    def __init__(self):
+        self.gps = 1234567890
+        self.segment_length = 10
+        self.post_trigger_length = 5
+        self.f_min = 20
+        self.f_max = 2000
+        self.ifos = [H1, L1]
+        self.f_ref = 100
+
+        self.likelihood = ZeroLikelihood()
+        self.prior = CombinePrior(
+            [
+                UniformPrior(1, 100,["mass1"]),
+                UniformPrior(1, 100,["mass2"]),
+            ]
+        )
+        self.sample_transforms = []
+        self.likelihood_transforms = []
+
+    def serialize(self, path: str = "./"):
+        """
+        Serialize a `Run` object into a human readble config file.
+        """
+        run_dict = {
+            "gps": self.gps,
+            "segment_length": self.segment_length,
+            "post_trigger_length": self.post_trigger_length,
+            "f_min": self.f_min,
+            "f_max": self.f_max,
+            "ifos": [ifo.name for ifo in self.ifos],
+            "f_ref": self.f_ref,
+        }
+        with open(path, "w") as f:
+            yaml.dump(run_dict, f)
+    
+    @classmethod
+    def deserialize(cls, path: str) -> SingleEventRun:
+        """
+        Deserialize a config file into a `Run` object
+        """
+        with open(path, "r") as f:
+            run_dict = yaml.safe_load(f)
+        run = cls()
+        run.gps = run_dict["gps"]
+        run.segment_length = run_dict["segment_length"]
+        run.post_trigger_length = run_dict["post_trigger_length"]
+        run.f_min = run_dict["f_min"]
+        run.f_max = run_dict["f_max"]
+        run.ifos = [detector_preset[ifo] for ifo in run_dict["ifos"]]
+        run.f_ref = run_dict["f_ref"]
+        return run
