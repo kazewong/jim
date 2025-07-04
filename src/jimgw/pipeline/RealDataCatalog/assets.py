2b0d797a9512eec348943de19242aaae7c956a2a
Kaze Wong <kazewong.physics@gmail.com>
2025-07-04 13:13:19 -0400

    Add asset for fetching list of events and downloading data + psd

--- src/jimgw/pipeline/RealDataCatalog/assets.py
+++ src/jimgw/pipeline/RealDataCatalog/assets.py
@@ -1,7 +1,63 @@
 import dagster as dg
-
+import gwosc
+import os
+from jimgw.core.single_event.data import Data
+import numpy as np
 
 # Create asset group for run and configuration0
+
+@dg.asset(
+    key_prefix="RealDataCatalog",
+    group_name="prerun",
+    description="Fetch all confident events and their gps time",
+)
+def event_list():
+    catalogs = ['GWTC-1-confident', 'GWTC-2.1-confident', 'GWTC-3-confident']
+    result = []
+    for catalog in catalogs:
+        event_list = gwosc.api.fetch_catalog_json(catalog)['events']
+        for event in event_list.values():
+            name = event['commonName']
+            gps_time = event['GPS']
+            result.append((name, gps_time))
+    with open("data/event_list.txt", "w") as f:
+        for name, gps_time in result:
+            f.write(f"{name} {gps_time}\n")
+
+
+@dg.multi_asset(
+    specs=[
+        dg.AssetSpec("RealDataCatalog_strain", deps=[event_list]),
+        dg.AssetSpec("RealDataCatalog_psd", deps=[event_list]),
+    ],
+    group_name="prerun",
+)
+def raw_data():
+    ifos = ["H1", "L1", "V1"]
+    with open("data/event_list.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            name, gps_time = line.strip().split()
+            start = float(gps_time) - 2
+            end = float(gps_time) + 2
+            event_dir = os.path.join("data", "raw", name)
+            os.makedirs(event_dir, exist_ok=True)
+            for ifo in ifos:
+                try:
+                    data = Data.from_gwosc(ifo, start, end)
+                    data.to_file(os.path.join(event_dir, f"{ifo}data"))
+                    psd_data = Data.from_gwosc(ifo, start - 512, end + 512)  # This needs to be changed at some point
+                    # psd_fftlength = data.duration * data.sampling_frequency  # Not used
+                    if np.isnan(psd_data.td).any():
+                        raise ValueError(
+                            f"PSD FFT length is NaN for {ifo}. "
+                            "This can happen when the selected time range contains contaminated data or missing data."
+                        )
+                    else:
+                        psd_data.to_file(os.path.join(event_dir, f"{ifo}psd"))
+                except Exception as e:
+                    print(f"Error fetching data for {ifo} during {name}: {e}")
+                    continue
 
 
 @dg.asset(
@@ -13,19 +69,7 @@ def config_file():
     pass
 
 
-@dg.multi_asset(
-    specs=[
-        dg.AssetSpec("RealDataCatalog_strain", deps=[config_file]),
-        dg.AssetSpec("RealDataCatalog_psd", deps=[config_file]),
-    ],
-    group_name="prerun",
-)
-def raw_data():
-    """
-    This is a placeholder function for the raw data.
-    It is used to demonstrate how to create a Dagster asset.
-    """
-    pass
+        
 
 
 @dg.multi_asset(
