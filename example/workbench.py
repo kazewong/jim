4d71f729ca9124b8d1d01298ce276691a2a3f21e
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2025-07-12 21:25:20 +0800

    Bugfix

--- example/workbench.py
+++ example/workbench.py
@@ -13,6 +13,7 @@ from jimgw.core.prior import (
     SinePrior,
     PowerLawPrior,
     UniformSpherePrior,
+    SimpleConstrainedPrior,
 )
 from jimgw.core.single_event.detector import get_H1, get_L1, get_V1
 from jimgw.core.single_event.likelihood import TransientLikelihoodFD
@@ -106,8 +107,8 @@ prior = prior + [
 ]
 
 # Extrinsic prior
-dL_prior = PowerLawPrior(100.0, 6000.0, 2.0, parameter_names=["d_L"])
-t_c_prior = UniformPrior(-0.1, 0.1, parameter_names=["t_c"])
+dL_prior = SimpleConstrainedPrior([PowerLawPrior(100.0, 6000.0, 2.0, parameter_names=["d_L"])])
+t_c_prior = SimpleConstrainedPrior([UniformPrior(-0.1, 0.1, parameter_names=["t_c"])])
 phase_c_prior = UniformPrior(0.0, 2 * jnp.pi, parameter_names=["phase_c"])
 psi_prior = UniformPrior(0.0, jnp.pi, parameter_names=["psi"])
 ra_prior = UniformPrior(0.0, 2 * jnp.pi, parameter_names=["ra"])
@@ -127,11 +128,9 @@ prior = CombinePrior(prior)
 # Defining Transforms
 
 sample_transforms = [
-    DistanceToSNRWeightedDistanceTransform(
-        gps_time=gps, ifos=ifos, dL_min=dL_prior.xmin, dL_max=dL_prior.xmax
-    ),
+    DistanceToSNRWeightedDistanceTransform(gps_time=gps, ifos=ifos),
     GeocentricArrivalPhaseToDetectorArrivalPhaseTransform(gps_time=gps, ifo=ifos[0]),
-    GeocentricArrivalTimeToDetectorArrivalTimeTransform(tc_min=t_c_prior.xmin, tc_max=t_c_prior.xmax, gps_time=gps, ifo=ifos[0]),
+    GeocentricArrivalTimeToDetectorArrivalTimeTransform(gps_time=gps, ifo=ifos[0]),
     SkyFrameToDetectorFrameSkyPositionTransform(gps_time=gps, ifos=ifos),
     BoundToUnbound(
         name_mapping=(["M_c"], ["M_c_unbounded"]),
