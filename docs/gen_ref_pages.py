cd9ab470876db86d04c9669f9632ba02039c9193
Kaze Wong <kazewong.physics@gmail.com>
2025-07-23 19:52:52 -0400

    update ignoring include

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -8,6 +8,10 @@ nav = mkdocs_gen_files.Nav()
 
 
 for path in sorted(Path("src").rglob("*.py")):  #
+    # Exclude files in src/jimgGw/run/cli
+    if path.is_relative_to(Path("src/jimgw/run/cli")):
+        continue
+
     module_path = path.relative_to("src").with_suffix("")  #
     doc_path = path.relative_to("src").with_suffix(".md")  #
     full_doc_path = Path("reference", doc_path)  #
